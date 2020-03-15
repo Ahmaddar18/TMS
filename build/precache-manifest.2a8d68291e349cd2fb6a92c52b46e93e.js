ce4b1f8ff600ddad6ee2576dc46a38cd90db9324
Ahmad <31131137+Ahmaddar18@users.noreply.github.com>
2020-03-15 23:04:23 +0100

    Add files via upload

---
+++ build/precache-manifest.2a8d68291e349cd2fb6a92c52b46e93e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3b847c3914e809c053373d5180c2bb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bdd7c91f9d2e646c725a",
+    "url": "/static/css/2.1bd2ab2f.chunk.css"
+  },
+  {
+    "revision": "c766c22de482063784b7",
+    "url": "/static/css/main.b458ccd4.chunk.css"
+  },
+  {
+    "revision": "bdd7c91f9d2e646c725a",
+    "url": "/static/js/2.8b8c2d66.chunk.js"
+  },
+  {
+    "revision": "97bd0f9563e702e79af257301c8a7a77",
+    "url": "/static/js/2.8b8c2d66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c766c22de482063784b7",
+    "url": "/static/js/main.241e2485.chunk.js"
+  },
+  {
+    "revision": "889bc3bb501a13fe03fb",
+    "url": "/static/js/runtime-main.8c9ca6ad.js"
+  }
+]);
